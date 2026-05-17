22d09732eb0e274b6ff45a4541b0028de7e113c4
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-05-18 02:00:23 +0800

    Add Native AOT publish support

---
+++ pack.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal
+
+pushd "%~dp0"
+
+set "CONFIGURATION=Release"
+set "ARTIFACTS_DIR=%CD%\artifacts"
+set "PACKAGES_DIR=%ARTIFACTS_DIR%\packages"
+
+if exist "%PACKAGES_DIR%" rmdir /s /q "%PACKAGES_DIR%"
+mkdir "%PACKAGES_DIR%"
+
+echo [1/3] Restoring solution...
+dotnet restore Zhijian.slnx
+if errorlevel 1 goto :error
+
+echo [2/3] Building solution...
+dotnet build Zhijian.slnx -c %CONFIGURATION% --no-restore
+if errorlevel 1 goto :error
+
+echo [3/3] Packing libraries...
+for %%P in (
+    "src\CodeWF.MindView\CodeWF.MindView.csproj"
+    "src\CodeWF.MindView.Themes\CodeWF.MindView.Themes.csproj"
+) do (
+    dotnet pack %%~P -c %CONFIGURATION% --no-build -o "%PACKAGES_DIR%"
+    if errorlevel 1 goto :error
+)
+
+echo.
+echo Packages are available in:
+echo %PACKAGES_DIR%
+
+popd
+exit /b 0
+
+:error
+set "EXIT_CODE=%ERRORLEVEL%"
+echo.
+echo Pack failed with exit code %EXIT_CODE%.
+popd
+exit /b %EXIT_CODE%
