e120e32f247b7135469713fb714bf417474a8394
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-05-21 22:15:18 +0800

    fix(changelog): improve markdown theme styling

--- src/Zhijian/Views/ChangelogWindow.axaml.cs
+++ src/Zhijian/Views/ChangelogWindow.axaml.cs
@@ -1,3 +1,6 @@
+using AtomUI.Controls;
+using AtomUI.Theme;
+using Avalonia;
 using Zhijian.ViewModels;
 using AtomWindow = AtomUI.Desktop.Controls.Window;
 
@@ -5,9 +8,41 @@ namespace Zhijian.Views;
 
 public partial class ChangelogWindow : AtomWindow
 {
+    private const string DarkClass = "dark";
+
     public ChangelogWindow()
     {
         InitializeComponent();
         DataContext = new ChangelogWindowViewModel();
+        SetDarkThemeClass(Application.Current?.IsDarkThemeMode() ?? false);
+
+        if (Application.Current?.GetThemeManager() is { } themeManager)
+        {
+            themeManager.BindingSource.PropertyChanged += HandleThemeManagerPropertyChanged;
+            Closed += (_, _) => themeManager.BindingSource.PropertyChanged -= HandleThemeManagerPropertyChanged;
+        }
+    }
+
+    private void HandleThemeManagerPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.Property == IThemeManager.IsDarkThemeModeProperty)
+        {
+            SetDarkThemeClass(e.GetNewValue<bool>());
+        }
+    }
+
+    private void SetDarkThemeClass(bool isDarkTheme)
+    {
+        if (isDarkTheme)
+        {
+            if (!Classes.Contains(DarkClass))
+            {
+                Classes.Add(DarkClass);
+            }
+
+            return;
+        }
+
+        Classes.Remove(DarkClass);
     }
 }
